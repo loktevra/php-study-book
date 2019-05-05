757c9b2308f8194f26af7154573df25764744ed7
Loktev Roman <loktevra@gmail.com>
2019-05-05 11:23:37 +0500

    simple api

--- docker-lamp/src/index.php
+++ docker-lamp/src/index.php
@@ -1,2 +1,44 @@
 <?php
-echo "Hello world";
+class Request {
+}
+
+class Response {
+  public function send($body) {
+    echo $body;
+  }
+}
+
+class Espresso {
+  private $routesGet = array();
+  private $routesPUT = array();
+  private $routesPOST = array();
+  private $routesDELETE = array();
+
+  public function get($path, $callback) {
+    $this->$routesGet[$path] = $callback;
+  }
+
+  public function run() {
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method == 'GET') {
+      $callback = $this->$routesGet[$_SERVER['REQUEST_URI']];
+      if (isset($callback)) {
+        $callback(new Request(), new Response());
+      } else {
+        echo $_SERVER['REQUEST_URI'];
+      }
+    }
+  }
+}
+
+$server = new Espresso();
+
+$server->get('/api/main', function ($req, $res) {
+  $res->send('Hello, world!');
+});
+$server->get('/api/sec', function ($req, $res) {
+  $res->send('Hello, sec!');
+});
+
+$server->run();
+
